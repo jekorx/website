3e4e1f836903ba833053ad838191f8d49a0bf9ee
wang_dgang <wdg404@163.com>
2019-03-01 15:34:54 +0800

    test bash

--- commit.sh
+++ commit.sh
@@ -11,8 +11,10 @@ echo 'blog.wdg.pub' > _book/CNAME
 
 git add .
 
+# 注释
 read -p "comment: " comment
 
 git commit -m "$comment"
 
+# 提交代码，删除gh-pages分支，重新提交gh-pages分支
 git push && git push origin --delete gh-pages && git subtree push --prefix _book origin gh-pages
