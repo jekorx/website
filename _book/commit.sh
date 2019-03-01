6d22722c5b3f23347658bf960cf9a985811bc62e
wang_dgang <wdg404@163.com>
2019-03-01 15:40:06 +0800

    bash

--- _book/commit.sh
+++ _book/commit.sh
@@ -16,5 +16,11 @@ read -p "comment: " comment
 
 git commit -m "$comment"
 
-# 提交代码，删除gh-pages分支，重新提交gh-pages分支
-git push && git push origin --delete gh-pages && git subtree push --prefix _book origin gh-pages
+# 提交代码
+git push
+
+# 删除gh-pages分支
+git push origin --delete gh-pages
+
+# 重新提交gh-pages分支
+git subtree push --prefix _book origin gh-pages
