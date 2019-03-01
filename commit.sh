2b629b8f87ee63a329d0b1b721e5b18eca6d327f
wang_dgang <wdg404@163.com>
2019-03-01 14:52:41 +0800

    test shell

---
+++ commit.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+
+# 当发生错误时中止脚本
+set -e
+
+# 构建
+gitbook build
+
+# 部署到自定义域域名
+echo 'blog.wdg.pub' > _book/CNAME
+
+git add .
+
+read -p "comment: " comment
+
+git commit -m "$comment"
+
+git push
